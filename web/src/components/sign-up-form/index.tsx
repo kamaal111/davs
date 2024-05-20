563b1b1fc22894be3d460c6503bd93bca67b21af
Kamaal Farah <kamaal.f1@gmail.com>
2024-05-20 20:58:24 +0000

    Collecting form data

--- web/src/components/sign-up-form/index.tsx
+++ web/src/components/sign-up-form/index.tsx
@@ -1,37 +1,65 @@
 'use client';
 
-import { Button, Card, Flex, Heading, Link, Text } from '@radix-ui/themes';
+import React from 'react';
+import { Button, Card, Flex, Heading, Text, Link } from '@radix-ui/themes';
 import { FormattedMessage, useIntl } from 'react-intl';
 
-import messages from './messages';
 import TextField from '../text-field';
+import messages from './messages';
 
 function SignUpForm() {
+  const [formData, setFormData] = React.useState({
+    username: '',
+    password: '',
+  });
+
   const intl = useIntl();
 
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    console.log('formData', formData);
+    clearForm();
+  }
+
+  function clearForm() {
+    setFormData({
+      username: '',
+      password: '',
+    });
+  }
+
+  function handleFieldChange(field: keyof typeof formData) {
+    return (value: string) => setFormData({ ...formData, [field]: value });
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <Card size="4">
         <Heading as="h3" size="6" trim="start" mb="5">
           <FormattedMessage {...messages.header} />
         </Heading>
 
         <TextField
+          value={formData.username}
           placeholder={intl.formatMessage(messages.usernameFieldPlaceholder)}
-          id="username-field"
+          id="username"
           label={() => (
-            <Text as="label" htmlFor="username-field" size="2" weight="bold">
+            <Text as="label" htmlFor="username" size="2" weight="bold">
               <FormattedMessage {...messages.usernameFieldLabel} />
             </Text>
           )}
+          onChange={handleFieldChange('username')}
         />
 
         <TextField
+          value={formData.password}
           placeholder={intl.formatMessage(messages.passwordFieldPlaceholder)}
-          id="password-field"
+          id="password"
+          type="password"
           label={() => (
             <>
-              <Text as="label" size="2" weight="bold" htmlFor="password-field">
+              <Text as="label" size="2" weight="bold" htmlFor="password">
                 <FormattedMessage {...messages.passwordFieldLabel} />
               </Text>
               <Link href="#" size="2" onClick={e => e.preventDefault()}>
@@ -39,10 +67,11 @@ function SignUpForm() {
               </Link>
             </>
           )}
+          onChange={handleFieldChange('password')}
         />
 
         <Flex mt="6" justify="end" gap="3">
-          <Button variant="outline">
+          <Button variant="outline" type="submit">
             <FormattedMessage {...messages.submitButton} />
           </Button>
           <FormattedMessage {...messages.signInButton} />
--- web/src/components/text-field/index.tsx
+++ web/src/components/text-field/index.tsx
@@ -1,21 +1,28 @@
 import React from 'react';
 import { Box, Flex, TextField as RadixTextField } from '@radix-ui/themes';
 
-function TextField({
-  label,
-  placeholder,
-  id,
-}: {
+type Props = {
   label: () => React.ReactNode;
   placeholder?: string;
   id?: string;
-}) {
+  onChange: (text: string) => void;
+  value: string;
+  type?: Parameters<typeof RadixTextField.Root>[0]['type'];
+};
+
+function TextField({ label, placeholder, id, onChange, value, type }: Props) {
   return (
     <Box mb="5" position="relative">
       <Flex align="baseline" justify="between" mb="1">
         {label()}
       </Flex>
-      <RadixTextField.Root placeholder={placeholder} id={id} />
+      <RadixTextField.Root
+        type={type}
+        value={value}
+        placeholder={placeholder}
+        id={id}
+        onChange={e => onChange(e.target.value)}
+      />
     </Box>
   );
 }
