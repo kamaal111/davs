618230c2841fc52b592e71d173c0ebe6f9073fcf
Kamaal Farah <kamaal.f1@gmail.com>
2024-05-26 14:02:00 +0000

    Generating messages keys constant

---
+++ web/codemod/transform-locales-constants.ts
@@ -0,0 +1,77 @@
+import {
+  API,
+  Collection,
+  FileInfo,
+  JSCodeshift,
+  VariableDeclarator,
+} from 'jscodeshift';
+
+function transform(fileInfo: FileInfo, api: API) {
+  const j = api.jscodeshift;
+
+  const source = j(fileInfo.source);
+  const messages = getMessages(source);
+  const messagesKeys = getMessagesKeys(j, messages);
+  const transformedKeys = transformMessagesKeys(messagesKeys);
+  const exportedMessagesKeysConstant = makeExportedMessagesKeysConstant(
+    j,
+    transformedKeys
+  );
+
+  return `${[exportedMessagesKeysConstant].join('\n\n')}\n`;
+}
+
+function makeExportedMessagesKeysConstant(
+  j: JSCodeshift,
+  transformedKeys: Record<string, string>
+) {
+  const properties = Object.entries(transformedKeys).map(([key, value]) => {
+    return j.property('init', j.identifier(key), j.literal(value));
+  });
+  const declaration = j.variableDeclaration('const', [
+    j.variableDeclarator(
+      j.identifier('MESSAGES_KEYS'),
+      j.objectExpression(properties)
+    ),
+  ]);
+  const exportedDeclaration = j.exportDeclaration(false, declaration);
+  let exportedDeclarationsSource = j(exportedDeclaration).toSource();
+  if (exportedDeclarationsSource.at(-1) === ';') {
+    exportedDeclarationsSource = exportedDeclarationsSource.slice(
+      0,
+      exportedDeclarationsSource.length - 1
+    );
+  }
+
+  return `${exportedDeclarationsSource} as const`;
+}
+
+function transformMessagesKeys(keys: string[]) {
+  return Object.fromEntries(keys.map(key => [key.replaceAll('.', '_'), key]));
+}
+
+function getMessagesKeys(
+  j: JSCodeshift,
+  messages: Collection<VariableDeclarator>
+) {
+  return messages
+    .find(j.Property)
+    .nodes()
+    .map(property => {
+      const { key } = property;
+      if (!j.Literal.check(key)) {
+        throw new Error('Invalid messages property key');
+      }
+      if (typeof key.value !== 'string') {
+        throw new Error('Invalid messages property key type');
+      }
+
+      return key.value;
+    });
+}
+
+function getMessages(source: Collection) {
+  return source.findVariableDeclarators('messages');
+}
+
+export default transform;
--- web/scripts/build-locales.ts
+++ web/scripts/build-locales.ts
@@ -1,4 +1,6 @@
 import { Glob } from 'bun';
+import path from 'node:path';
+import fs from 'node:fs/promises';
 
 import { extract } from '@formatjs/cli-lib';
 import unflatten from '@kamaalio/kamaal/objects/unflatten';
@@ -7,20 +9,84 @@ import { run as jscodeshift } from 'jscodeshift/src/Runner';
 type MessageDescriptor = { defaultMessage: string };
 type ExtractedMessages = Record<string, MessageDescriptor>;
 
+const GENERATED_CONSTANTS_FILE_PATH =
+  'scripts/build-locales-generated-constants.ts';
+
+const CODEMOD_TIMEOUT_IN_MILLISECONDS = 1500;
+
+class TimeoutError extends Error {
+  constructor(millis: number) {
+    super(`Timed out after ${millis} ms.`);
+  }
+}
+
 async function main() {
   const messagesFiles = await getMessagesFiles();
   const messages = await extractDefaultMessages(messagesFiles);
-  makeConstants(messages.flatMessages);
 
-  await Bun.write(
-    'src/translations/messages/en.json',
-    appendNewLineToString(JSON.stringify(messages.unflattenMessages, null, 2))
+  await Promise.all(
+    [
+      {
+        destination: GENERATED_CONSTANTS_FILE_PATH,
+        data: `const messages = ${JSON.stringify(messages.flatMessages, null, 2)}`,
+      },
+      {
+        destination: 'src/translations/messages/en.json',
+        data: appendNewLineToString(
+          JSON.stringify(messages.unflattenMessages, null, 2)
+        ),
+      },
+    ].map(({ destination, data }) => Bun.write(destination, data))
+  );
+
+  let error: Error | undefined;
+  try {
+    await makeConstants();
+  } catch (e) {
+    error = e as Error;
+  }
+
+  const generatedFile = Bun.file(GENERATED_CONSTANTS_FILE_PATH);
+  if (error == null || error instanceof TimeoutError) {
+    await Bun.write('src/translations/messages/constants.ts', generatedFile);
+  }
+
+  if (error != null) {
+    await fs.unlink(GENERATED_CONSTANTS_FILE_PATH);
+    throw error;
+  }
+}
+
+async function makeConstants() {
+  return await withTimeout(
+    CODEMOD_TIMEOUT_IN_MILLISECONDS,
+    jscodeshift(
+      path.resolve('codemod/transform-locales-constants.ts'),
+      [path.resolve(GENERATED_CONSTANTS_FILE_PATH)],
+      {
+        dry: false,
+        verbose: 1,
+        print: true,
+        parser: 'typescript',
+      }
+    )
   );
 }
 
-function makeConstants(messages: Record<string, string>) {
-  console.log('messages', messages);
-  console.log('jscodeshift', jscodeshift);
+async function withTimeout<Result>(
+  millis: number,
+  promise: Promise<Result>
+): Promise<Result> {
+  let timeoutPid: Timer;
+  const timeout = new Promise(
+    (_resolve, reject) =>
+      (timeoutPid = setTimeout(() => reject(new TimeoutError(millis)), millis))
+  );
+  return (Promise.race([promise, timeout]) as Promise<Result>).finally(() => {
+    if (timeoutPid) {
+      clearTimeout(timeoutPid);
+    }
+  });
 }
 
 function appendNewLineToString(value: string) {
