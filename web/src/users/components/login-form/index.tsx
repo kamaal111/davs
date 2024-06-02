cb02550df5e175c7998ea2b3c85adcfa1b57bf37
Kamaal Farah <kamaal.f1@gmail.com>
2024-06-02 15:44:12 +0000

    Creating username input for login

--- web/src/users/components/login-form/index.tsx
+++ web/src/users/components/login-form/index.tsx
@@ -1,13 +1,30 @@
 'use client';
 
-import { Card, Heading } from '@radix-ui/themes';
-import { FormattedMessage } from 'react-intl';
+import React from 'react';
+import { Card, Heading, Text } from '@radix-ui/themes';
+import { FormattedMessage, useIntl } from 'react-intl';
 
 import messages from './messages';
+import TextField from '@/components/text-field';
+
+type FormInput = { username: string; password: string };
 
 function LoginForm() {
+  const [formData, setFormData] = React.useState<FormInput>({
+    username: '',
+    password: '',
+  });
+
+  const intl = useIntl();
+
   async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
+  }
+
+  function handleFieldChange(key: keyof FormInput) {
+    return (value: string) => {
+      setFormData({ ...formData, [key]: value });
+    };
   }
 
   return (
@@ -16,6 +33,17 @@ function LoginForm() {
         <Heading as="h3" size="6" trim="start" mb="5">
           <FormattedMessage {...messages.header} />
         </Heading>
+        <TextField
+          value={formData.username}
+          placeholder={intl.formatMessage(messages.usernameFieldPlaceholder)}
+          id="username"
+          label={() => (
+            <Text as="label" htmlFor="username" size="2" weight="bold">
+              <FormattedMessage {...messages.usernameFieldLabel} />
+            </Text>
+          )}
+          onChange={handleFieldChange('username')}
+        />
       </Card>
     </form>
   );
--- web/src/users/components/login-form/messages.ts
+++ web/src/users/components/login-form/messages.ts
@@ -6,6 +6,16 @@ const messages = defineMessages({
     defaultMessage: 'Login',
     description: 'Login form header',
   },
+  usernameFieldLabel: {
+    id: 'LOGIN_FORM.USERNAME_FIELD_LABEL',
+    defaultMessage: 'Username',
+    description: 'Login forms username field label',
+  },
+  usernameFieldPlaceholder: {
+    id: 'LOGIN_FORM.USERNAME_FIELD_PLACEHOLDER',
+    defaultMessage: 'Enter your username',
+    description: 'Login forms username field placeholder',
+  },
 });
 
 export default messages;
