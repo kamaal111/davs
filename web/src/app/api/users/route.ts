ad5824486cc4203e17f72dd751016b4ca672d8ac
Kamaal Farah <kamaal.f1@gmail.com>
2024-06-02 13:39:10 +0000

    Moving user route handler to users feature

--- web/src/app/api/users/route.ts
+++ web/src/app/api/users/route.ts
@@ -1,41 +1,7 @@
 import type { NextRequest } from 'next/server';
-import type { z } from 'zod';
 
-import signUpPayload from '@/users/validators/sign-up-payload';
-import InvalidSignUpPayloadError from './errors/invalid-sign-up-payload-error';
-import apiErrorHandler from '@/common/errors/api-error-handler';
-import parseAPIPayload from '@/common/api/parse-api-payload';
-import encryption from '@/encryption/encryption';
-import METHODS from '@/common/http/methods';
-import loadRequiredEnv from '@/common/env/load-required-env';
-import UserAlreadyExists from './errors/user-already-exists';
-import FailedToCreateUser from './errors/failed-to-create-user';
-
-const { DAVS_API_KEY, DAVS_SERVER_BASE_URL } = loadRequiredEnv([
-  'DAVS_API_KEY',
-  'DAVS_SERVER_BASE_URL',
-]);
+import signUpHandler from '@/users/server/route-handlers/sign-up-handler';
 
 export function POST(request: NextRequest) {
-  return apiErrorHandler(async () => {
-    let body: z.infer<typeof signUpPayload>;
-    try {
-      body = await parseAPIPayload(request, signUpPayload);
-    } catch (error) {
-      throw new InvalidSignUpPayloadError(request, { cause: error as Error });
-    }
-
-    const encryptedBody = encryption.aes.encryptObject(body);
-    const response = await fetch(`${DAVS_SERVER_BASE_URL}/api/v1/users`, {
-      method: METHODS.POST,
-      body: JSON.stringify({ message: encryptedBody }),
-      headers: { authorization: `Token ${DAVS_API_KEY!}` },
-    });
-    if (!response.ok) {
-      if (response.status === 409) throw new UserAlreadyExists(request);
-      else throw new FailedToCreateUser(request, response.status);
-    }
-
-    return Response.json({ details: 'OK' }, { status: 201 });
-  });
+  return signUpHandler(request);
 }
---
+++ web/src/users/server/route-handlers/sign-up-handler.ts
@@ -0,0 +1,45 @@
+'use server';
+
+import type { z } from 'zod';
+import type { NextRequest } from 'next/server';
+
+import apiErrorHandler from '@/common/errors/api-error-handler';
+import signUpPayload from '../../validators/sign-up-payload';
+import parseAPIPayload from '@/common/api/parse-api-payload';
+import InvalidSignUpPayloadError from '@/users/server/errors/invalid-sign-up-payload-error';
+import encryption from '@/encryption/encryption';
+import loadRequiredEnv from '@/common/env/load-required-env';
+import METHODS from '@/common/http/methods';
+import FailedToCreateUser from '../errors/failed-to-create-user';
+import UserAlreadyExists from '../errors/user-already-exists';
+
+const { DAVS_API_KEY, DAVS_SERVER_BASE_URL } = loadRequiredEnv([
+  'DAVS_API_KEY',
+  'DAVS_SERVER_BASE_URL',
+]);
+
+function signUpHandler(request: NextRequest) {
+  return apiErrorHandler(async () => {
+    let body: z.infer<typeof signUpPayload>;
+    try {
+      body = await parseAPIPayload(request, signUpPayload);
+    } catch (error) {
+      throw new InvalidSignUpPayloadError(request, { cause: error as Error });
+    }
+
+    const encryptedBody = encryption.aes.encryptObject(body);
+    const response = await fetch(`${DAVS_SERVER_BASE_URL}/api/v1/users`, {
+      method: METHODS.POST,
+      body: JSON.stringify({ message: encryptedBody }),
+      headers: { authorization: `Token ${DAVS_API_KEY!}` },
+    });
+    if (!response.ok) {
+      if (response.status === 409) throw new UserAlreadyExists(request);
+      else throw new FailedToCreateUser(request, response.status);
+    }
+
+    return Response.json({ details: 'OK' }, { status: 201 });
+  });
+}
+
+export default signUpHandler;
