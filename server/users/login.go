d84cd88f9acc81b26f2d1397e9cc88573cf01796
Kamaal Farah <kamaal.f1@gmail.com>
2024-06-09 13:04:49 +0000

    Decrypting login payload

--- server/users/login.go
+++ server/users/login.go
@@ -1,16 +1,67 @@
 package users
 
 import (
+	"encoding/json"
+	"fmt"
+	"log"
 	"net/http"
+	"os"
 
+	ginErrors "github.com/Kamaalio/kamaalgo/gin/errors"
+	kamaalStrings "github.com/Kamaalio/kamaalgo/strings"
 	"github.com/gin-gonic/gin"
+	"github.com/kamaal111/davs/crypto"
+	"github.com/kamaal111/davs/utils"
 	"gorm.io/gorm"
 )
 
 func loginHandler(db *gorm.DB) func(context *gin.Context) {
 	return func(context *gin.Context) {
+		payload, handled := utils.ValidatePayload[loginPayload](context)
+		if handled {
+			return
+		}
+
+		encryptionKey, err := kamaalStrings.Unwrap(os.Getenv("ENCRYPTION_SECRET_KEY"))
+		if err != nil {
+			log.Printf("Failed to get encryption key; error=%v", err)
+			ginErrors.ErrorHandler(context, ginErrors.Error{Message: "Something went wrong", Status: http.StatusInternalServerError})
+			return
+		}
+
+		decryptedPayload, err := crypto.AESDecrypt([]byte(encryptionKey), []byte(payload.Message))
+		if err != nil {
+			ginErrors.ErrorHandler(context, ginErrors.Error{
+				Message: "Invalid body provided",
+				Status:  http.StatusBadRequest,
+			})
+			return
+		}
+
+		var decryptedPayloadJSON decryptedLoginPayload
+		err = json.Unmarshal(decryptedPayload, &decryptedPayloadJSON)
+		if err != nil {
+			ginErrors.ErrorHandler(context, ginErrors.Error{
+				Message: "Invalid body provided",
+				Status:  http.StatusBadRequest,
+			})
+			return
+		}
+
+		user := User{Username: decryptedPayloadJSON.Username, Password: decryptedPayloadJSON.Password}
+		fmt.Println(user)
+
 		context.JSON(http.StatusOK, loginResponse{Details: "Created"})
 	}
+}
+
+type loginPayload struct {
+	Message string `json:"message" binding:"required,min=10"`
+}
+
+type decryptedLoginPayload struct {
+	Username string `json:"username" binding:"required,min=1"`
+	Password string `json:"password" binding:"required,min=5"`
 }
 
 type loginResponse struct {
