b2020db8d0f3305b423cc5ef4e387044ab095cc4
Kamaal Farah <kamaal.f1@gmail.com>
2024-07-06 13:17:20 +0200

    Removing encryption from sign-up

--- server/users/sign-up.go
+++ server/users/sign-up.go
@@ -1,13 +1,11 @@
 package users
 
 import (
-	"encoding/json"
 	"log"
 	"net/http"
 
 	ginErrors "github.com/Kamaalio/kamaalgo/gin/errors"
 	"github.com/gin-gonic/gin"
-	"github.com/kamaal111/davs/crypto"
 	"github.com/kamaal111/davs/utils"
 	"gorm.io/gorm"
 )
@@ -19,28 +17,8 @@ func signUpHandler(db *gorm.DB) func(context *gin.Context) {
 			return
 		}
 
-		environment := utils.GetEnvironment()
-		decryptedPayload, err := crypto.AESDecrypt([]byte(environment.EncryptionSecretKey), []byte(payload.Message))
-		if err != nil {
-			ginErrors.ErrorHandler(context, ginErrors.Error{
-				Message: "Invalid body provided",
-				Status:  http.StatusBadRequest,
-			})
-			return
-		}
-
-		var decryptedPayloadJSON decryptedSignUpPayload
-		err = json.Unmarshal(decryptedPayload, &decryptedPayloadJSON)
-		if err != nil {
-			ginErrors.ErrorHandler(context, ginErrors.Error{
-				Message: "Invalid body provided",
-				Status:  http.StatusBadRequest,
-			})
-			return
-		}
-
-		user := User{Username: decryptedPayloadJSON.Username, Password: decryptedPayloadJSON.Password}
-		err = user.create(db)
+		user := User{Username: payload.Username, Password: payload.Password}
+		err := user.create(db)
 		if err != nil {
 			if err == errUserExists {
 				ginErrors.ErrorHandler(context, ginErrors.Error{
@@ -85,10 +63,6 @@ type signUpResponse struct {
 }
 
 type signUpPayload struct {
-	Message string `json:"message" binding:"required,min=10"`
-}
-
-type decryptedSignUpPayload struct {
 	Username string `json:"username" binding:"required,min=1"`
 	Password string `json:"password" binding:"required,min=5"`
 }
