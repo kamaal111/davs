8b9b56104279b0e48a707deae27021d7e86b9a9d
Kamaal Farah <kamaal.f1@gmail.com>
2024-10-27 15:16:29 +0000

    Upgrading packages

--- web/eslint.config.js
+++ web/eslint.config.js
@@ -6,7 +6,7 @@ import tseslint from 'typescript-eslint';
 import reactCompiler from 'eslint-plugin-react-compiler';
 
 export default tseslint.config(
-  { ignores: ['dist', '.next'] },
+  { ignores: ['dist', '.next', 'coverage'] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
